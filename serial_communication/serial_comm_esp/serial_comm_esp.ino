c1e1b21608ff67648d577efcb6c2039352478be0
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-01-28 23:45:26 +0530

    Update serial_comm_esp.ino

--- serial_communication/serial_comm_esp/serial_comm_esp.ino
+++ serial_communication/serial_comm_esp/serial_comm_esp.ino
@@ -1,12 +1,45 @@
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiClient.h>
+
+ESP8266WebServer server(80);
 SoftwareSerial s(D6, D5);
 
+const char* ssid = "wifi_ssid";
+const char* password = "password";
+
 int data;
+String page = "";
 
 void setup()
 {
   s.begin(9600);
   Serial.begin(9600);
+
+  Serial.println("Connecting to");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.....!");
+  Serial.println("Got IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  server.on("/", [](){
+    page = "<h1>Sensor to Node MCU Web Server</h1><h3>Data:</h3> <h4>"+String(data)+"</h4>";
+    server.send(200, "text/html", page);
+  });
 }
 
 void loop()
@@ -18,4 +51,6 @@ void loop()
     data = s.read();
     Serial.println(data);
   }
+//  delay(1000);
+  server.handleClient();
 }
